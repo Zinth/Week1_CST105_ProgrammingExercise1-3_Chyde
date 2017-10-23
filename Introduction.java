c7f29534e638d6aecb4ed20a5295d67c08603c65
CHyde <blimple10@gmail.com>
2017-10-23 15:28:01 -0700

    Introduction
    Programming Exercise 1-3 submission.

---
+++ Introduction.java
@@ -0,0 +1,14 @@
+/** Program: Introduction Program
+* File: Introduction.java
+* Summary: Created to complete Week 1: Programming Exercise 3 
+* Author: Chris Hyde
+* Date: October 19, 2017 
+**/
+
+public class Introduction {
+
+	public static void main(String[] args) {
+
+	}
+
+}
